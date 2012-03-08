ed13e04a83d95f3024cb77a62d64c24c8143d79b
DonMilligan <don.milligan@itas.ca>
2012-03-08 11:15:11 -0800

    First commit.

---
+++ medds.carecenter.dev/src/medds/carecenter/devActivity.java
@@ -0,0 +1,14 @@
+package medds.carecenter;
+
+import medds.carecenter.dev.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class devActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
